a9c23fa7c450528c854cb4ca37cca7b892775f70
Bao Chau <bchau@promotal.com>
2024-07-06 12:06:15 +0200

    added logger

--- src/monitor/monitor.controller.ts
+++ src/monitor/monitor.controller.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Body, Controller, NotFoundException, Post, Query } from '@nestjs/common'
+import { BadRequestException, Body, Controller, Logger, NotFoundException, Post, Query } from '@nestjs/common'
 import { BotService } from '@/bot/bot.service'
 import { TrackTransactionDto } from './dto/track-transaction.dto'
 import { eventWatchList } from './utils/events'
@@ -6,6 +6,8 @@ import { TrackService } from '@/database/services/track.service'
 
 @Controller('monitors')
 export class MonitorController {
+  private logger = new Logger('MonitorController')
+
   constructor(
     private readonly botService: BotService,
     private readonly trackService: TrackService
@@ -14,6 +16,7 @@ export class MonitorController {
   @Post('transactions')
   async trackTransactions(@Body() body: TrackTransactionDto, @Query('userId') userId: string) {
     const address = body.triggered_for
+    this.logger.log('🚀 ~ MonitorController ~ address:', address)
     if (!address) throw new BadRequestException('Missing triggered for')
 
     if (!body.type || !body.actions || !eventWatchList.includes(body.type)) {
@@ -29,8 +32,9 @@ export class MonitorController {
     if (!tokensSwapped) throw new BadRequestException('Invalid callback data')
 
     const track = await this.trackService.getByUserId(userId)
+    this.logger.log(track)
     const wallet = track.trackedWallets.find(({ wallet }) => wallet.address === address)
-
+    this.logger.log(wallet)
     if (!wallet) throw new NotFoundException(`Not found wallet with address ${address}`)
 
     this.botService.notifyTransaction({
